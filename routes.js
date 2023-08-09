ad2686df296418bb790ed2453a78600fa05d0dcf
Akshaymonkv <70555498+Akshaymonkv@users.noreply.github.com>
2023-08-10 01:28:42 +0530

    Defining controller and routes.

---
+++ routes.js
@@ -0,0 +1,17 @@
+//defining the routes as a seperate file and then will go to the CRUD operations from here
+const express = require('express');         //requiring express into router file
+const router = express.Router();            //Calling the router function
+
+const  {
+    readTodo,
+    updateTodo,
+    deleteTodo,
+    createTodo
+} = require('./controller');               //declaring routes          
+
+router.get('/todos', readTodo);
+router.post('/todos/post',createTodo);
+router.put('todos/update/:id', updateTodo);
+router.delete('todos/delete/:id',deleteTodo);
+
+module.exports = router;
